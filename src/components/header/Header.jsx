0cb6c9f261205b4538555b6759ecc12034e0d4d9
Manuel-AC-Ventura <nelo.manuel.790@gmail.com>
2023-12-30 16:20:32 +0100

    Correção de bug na exibição de imagem

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,11 @@
+import michaelJordanImage from "/image-michael-jordan.png"
 
 export const Header = ()=>{
+  const style = {
+    backgroundImage: `url(${michaelJordanImage})`
+  }
   return(
-    <header className="w-full h-80 bg-[url(image-michael-jordan.png)] bg-center bg-cover text-white flex items-center max-sm:pl-5 max-sm:h-96 max-sm:bg-cover md:pl-20 lg:pl-60">
+    <header style={style} className="w-full h-80 bg-center bg-cover text-white flex items-center max-sm:pl-5 max-sm:h-96 max-sm:bg-cover md:pl-20 lg:pl-60">
       <nav className=" w-96 flex gap-3 flex-col justify-center">
         <p className="flex gap-2 items-center">
           <img className="h-4" src="logo.svg" alt="logo" />
